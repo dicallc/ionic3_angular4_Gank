920763b7391db7cb59584d3c36b925d694795ae7
蒋迪凯 <jiangdk@e-lead.cn>
2017-06-17 11:43:35 +0800

    格式化时间

--- src/providers/http-service.ts
+++ src/providers/http-service.ts
@@ -14,10 +14,19 @@ export class HttpServiceProvider {
   }
   public getCategoryLists(str): Observable<string> {
     return this.http.get(CATEGORY_LIST_HEAD_URL+str+CATEGORY_LIST_END_PARAMS)
-      .map(res => res.json().results);
+      .map(res =>this.mapFucntion(res));
     // return this.http.get(CATEGORY_LIST_HEAD_URL+str+CATEGORY_LIST_END_PARAMS)
     //   .map(this.extractData)
     //   .catch(this.handleError);
   }
 
+  private mapFucntion(res):any {
+    var actors:any[]=res.json().results;
+    for (var i = 0; i < actors.length; i ++) {
+      var str:string= actors[i].publishedAt;
+      actors[i].publishedAt= str.substring(0,10)
+    }
+    return  actors;
+
+  }
 }
